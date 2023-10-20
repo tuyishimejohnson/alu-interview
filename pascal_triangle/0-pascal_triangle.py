a49833f41fe8d7e0c7001b0a14ddff780713549f
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-10-20 11:29:08 +0200

    Pascal's Triangle

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
+import math
 
 '''Creating a function that returns a list of lists of integers representing the Pascal's triangle '''
 
 def pascal_triangle(n):
     '''n will always be an integer '''
-    n = int(n)
-    my_list = []
-    for i in range(n):
-        if n <= 0:
-            return []
-        return my_list.append(i)    
+    my_list = int(n)
+    list_length = len(my_list)
+    number = math.factorial(my_list)/math.factorial(list_length)*math.factorial(my_list - list_length)
+    return number
+
