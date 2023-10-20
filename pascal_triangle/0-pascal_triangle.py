944220d7594eb0c64e06cb65563ef9815f04d0ae
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-10-20 12:15:48 +0200

    Pascal's Triangle

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -4,14 +4,12 @@
 
 def pascal_triangle(n):
 
-
     '''returning empty list if n is less than or equal to 0'''
-
     if n <= 0:
         return []
 
     this_triangle = [[1]]
-    for i in range(1,n):
+    for i in range(1, n):
         row = [1]
         for j in range(1, i):
             row.append(this_triangle[i-1][j-1] + this_triangle[i-1][j])
