c4ceaeea890b0512b0a0f9040dc5015e40cae515
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-10-20 12:05:50 +0200

    Pascal's Triangle

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 
-'''Creating a function that returns a list of lists of integers representing the Pascal's triangle '''
+''' Function that creates Pascal Triangle '''
 
-def pascal_triangle(n):
-    '''n will always be an integer '''
-    for num in range(n):
-        if n <= 0:
-            print([])
-        print(' '*(n-num), end='')
-        print()
-        print(' '.join(map(str, str(11 ** num))))
+def generate_pascal_triangle(n):
+    '''returning empty list if n is less than or equal to 0'''
 
+    if n <= 0:
+        return []
+
+    pascal_triangle = [[1]]
+    for i in range(1,n):
+        row = [1]
+        for j in range(1, i):
+            row.append(pascal_triangle[i-1][j-1] + pascal_triangle[i-1][j])
+        row.append(1)
+        pascal_triangle.apped(row)
+
+    return pascal_triangle
